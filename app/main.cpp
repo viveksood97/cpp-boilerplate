a73c67d74e2529b04db53483847f288854f975aa
Daniel M. Lofaro <dan@danlofaro.com>
2020-10-12 22:38:18 -0400

    added boost example

--- app/main.cpp
+++ app/main.cpp
@@ -1,15 +1,51 @@
-#include <string>
+// Copyright Vladimir Prus 2002-2004.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+/* The simplest usage of the library.
+ */
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
 #include <iostream>
-#include <Eigen/Dense>
+#include <iterator>
+using namespace std;
 
-int main()
+int main(int ac, char* av[])
 {
-	Eigen::Matrix3d M = Eigen::Matrix3d::Random();
-	M = (M + Eigen::Matrix3d::Constant(1.2)) * 50;
+    try {
 
-	std::cout << "M = " << std::endl << M << std::endl;
+        po::options_description desc("Allowed options");
+        desc.add_options()
+            ("help", "produce help message")
+            ("compression", po::value<double>(), "set compression level")
+        ;
 
-	Eigen::Vector4d v(1, 2, 3, 4);
-	std::cout << "M*v = " << std::endl << M * v << std::endl;
+        po::variables_map vm;        
+        po::store(po::parse_command_line(ac, av, desc), vm);
+        po::notify(vm);    
+
+        if (vm.count("help")) {
+            cout << desc << "\n";
+            return 0;
+        }
+
+        if (vm.count("compression")) {
+            cout << "Compression level was set to " 
+                 << vm["compression"].as<double>() << ".\n";
+        } else {
+            cout << "Compression level was not set.\n";
+        }
+    }
+    catch(exception& e) {
+        cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        cerr << "Exception of unknown type!\n";
+    }
+
     return 0;
 }
